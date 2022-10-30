e2fd827f8448522bfbe41f2b877c9cd6ac85eddc
Victor5g <vicoe.sales@gmail.com>
2022-10-30 09:41:53 -0300

    Feat: Add App-Icon

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,8 +1,8 @@
-import banner from "../assets/images/banner.png";
-import bitcoin from "../assets/images/bitcoin.png";
-import ethereum from "../assets/images/ethereum.png";
-import litecoin from "../assets/images/litecoin.png";
-import ripple from "../assets/images/ripple.png";
+import banner from "../assets/images/illustration/banner.png";
+import bitcoin from "../assets/images/illustration/bitcoin.png";
+import ethereum from "../assets/images/illustration/ethereum.png";
+import litecoin from "../assets/images/illustration/litecoin.png";
+import ripple from "../assets/images/illustration/ripple.png";
 
 export default {
   banner,
